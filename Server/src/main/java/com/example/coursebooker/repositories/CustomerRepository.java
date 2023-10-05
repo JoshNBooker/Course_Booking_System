9e6c05958f86cdc3f63334587425bf82530eee8d
Josh Booker <josh.booker2@gmail.com>
2023-10-05 13:36:48 +0100

    made customer query ignore case

--- Server/src/main/java/com/example/coursebooker/repositories/CustomerRepository.java
+++ Server/src/main/java/com/example/coursebooker/repositories/CustomerRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    List<Customer> findByBookingsCourseName(String name);
+    List<Customer> findByBookingsCourseNameIgnoreCase(String name);
 
 
 }
